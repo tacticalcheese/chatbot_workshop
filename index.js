70ff309956f102f1e6a524e34980923be3aecd13
Pascal Dawson <dawsonpascal@hotmail.com>
2020-01-23 14:39:05 +0100

    what is the problem?

--- index.js
+++ index.js
@@ -8,36 +8,39 @@ app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
 function handleMessage(sender_psid, received_message) {
   let response;
-  if (received_message.text) {    
+  if (received_message.text) {
     response = {
-      "text": `You sent the message: "${received_message.text}". Now send me an image!`
-    }
-  }  
-  callSendAPI(sender_psid, response); 
+      text: `You sent the message: "${received_message.text}". Now send me an image!`
+    };
+  }
+  callSendAPI(sender_psid, response);
 }
 
 function handlePostback(sender_psid, received_postback) {}
 
 function callSendAPI(sender_psid, response) {
   let request_body = {
-    "recipient": {
-      "id": sender_psid
+    recipient: {
+      id: sender_psid
     },
-    "message": response
-  }
+    message: response
+  };
 
-  request({
-    "uri": "https://graph.facebook.com/v2.6/me/messages",
-    "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
-    "method": "POST",
-    "json": request_body
-  }, (err, res, body) => {
-    if (!err) {
-      console.log('message sent!')
-    } else {
-      console.error("Unable to send message:" + err);
+  request(
+    {
+      uri: 'https://graph.facebook.com/v2.6/me/messages',
+      qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+      method: 'POST',
+      json: request_body
+    },
+    (err, res, body) => {
+      if (!err) {
+        console.log('message sent!');
+      } else {
+        console.error('Unable to send message:' + err);
+      }
     }
-  }); 
+  );
 }
 
 let logs = [];
@@ -47,7 +50,7 @@ app.post('/webhook', (req, res) => {
   if (body.object === 'page') {
     body.entry.forEach(function(entry) {
       let webhook_event = entry.messaging[0];
-      logs.push(webhook_event)
+      logs.push(webhook_event);
       console.log(webhook_event);
       let sender_psid = webhook_event.sender.id;
       console.log('Sender PSID: ' + sender_psid);
