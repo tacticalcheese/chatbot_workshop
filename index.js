fa7f33556568043864dbc8ad254e95bd6055f858
Pascal Dawson <dawsonpascal@hotmail.com>
2020-01-23 14:25:28 +0100

    still shit

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ app.post('/webhook', (req, res) => {
   if (body.object === 'page') {
     body.entry.forEach(function(entry) {
       let webhook_event = entry.messaging[0];
+      logs.push(webhook_event)
       console.log(webhook_event);
       let sender_psid = webhook_event.sender.id;
       console.log('Sender PSID: ' + sender_psid);
